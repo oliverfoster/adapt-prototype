836a8abff101402c27f329c28920c6bc4ca12815
oliver foster <oliver.foster>
2015-06-24 23:57:39 +0100

    Update

--- js/adapt-prototype.js
+++ js/adapt-prototype.js
@@ -1,16 +1,86 @@
 define([
 	"coreJS/adapt"
 ], function(Adapt) {
+
+	var EditView = Backbone.View.extend({
+		$currentComponentContainer: null,
+		currentComponentID: null,
+		events: {
+			"click": "onEditClicked"
+		},
+		initialize: function() {
+			$("body").append(this.el);
+		},
+		onEditClicked: function(event) {
+			event.preventDefault();
+			this.openEditWindow();
+		},
+		onResize: function() {
+			if (!this.$currentComponentContainer) return;
+
+			var offset = this.$currentComponentContainer.offset();
+			editView.$el.css({
+				top: offset['top']+"px",
+				right: offset['left']+"px",
+				display: "block"
+			});
+		},
+		openEditWindow: function() {
+			var prototypeWindow = window.open("prototype/index.html", "_blank");
+			prototypeWindow.Adapt = Adapt;
+			prototypeWindow.data = {
+				view: start.findViewById(this.currentComponentID)
+			};
+			prototypeWindow.parentWindow = window;
+		}
+	});
 	
 	
 
 	var StartView = Backbone.View.extend({
+		views: [],
+		events: {
+			"mouseover .prototype-component": "onComponentFocus",
+			"focus .prototype-component": "onComponentFocus"
+		},
 		initialize: function() {
-			var prototypeWindow = window.open("prototype/index.html", "_blank");
-			prototypeWindow.Adapt = Adapt;
+			this.setupEventListeners();
+		},
+		setupEventListeners: function() {
+			this.listenTo(Adapt, "componentView:preRender", this.onComponentPreRender);
+			this.listenTo(Adapt, "remove", this.onRemove);
+		},
+		onComponentPreRender: function(view) {
+			this.views.push(view);
+			view.$el.addClass("prototype-component").attr("data-prototype-id", view.model.get("_id"));
+		},
+		onComponentFocus: function(event) {
+			var $componentContainer = $(event.currentTarget);
+			var id = $componentContainer.attr("data-prototype-id");
+			editView.currentComponentID = id;
+			editView.$currentComponentContainer = $componentContainer;
+			editView.onResize();
+		},
+		onRemove: function() {
+			this.views = [];
+		},
+		findViewById: function(id) {
+			for (var i = 0, l = this.views.length; i < l; i++) {
+				if (this.views[i].model.get("_id") == id) {
+					return this.views[i];
+				}
+			}
+			return undefined;
 		}
 	});
 
-	var start = new StartView();
+	var start;
+	var editView;
+
+	Adapt.once("app:dataReady", function() {
+
+		start = new StartView({ el: document.getElementsByTagName("body")[0] });
+		editView = new EditView({ el: $(Handlebars.templates['prototype-edit']({}))[0] })
+	});
 
 });
--- required/prototype/js/main.js
+++ required/prototype/js/main.js
@@ -1,1 +1,66 @@
-var parentWindow = window.top;
+$(function(){ 
+  // This is the starting value for the editor
+      // We will use this to seed the initial editor 
+      // and to provide a "Restore to Default" button.
+      var starting_value = data.view.model.toJSON();
+      
+      // Initialize the editor
+      var editor = new JSONEditor(document.getElementById('editor_holder'),{
+        // Enable fetching schemas via ajax
+        ajax: true,
+        
+        // The schema for the editor
+        schema: {
+          "$ref": "schemas/" + data.view.model.get("_component") + ".json"
+        },
+        
+        // Seed the form with a starting value
+        startval: starting_value,
+
+        // Disable additional properties
+        no_additional_properties: true,
+        
+        // Require all properties by default
+        required_by_default: true
+      });
+      
+      // Hook up the submit button to log to the console
+      document.getElementById('update').addEventListener('click',function() {
+        // Get the value from the editor
+        
+        var updated = editor.getValue();
+
+        for (var k in updated) {
+          data.view.model.set(k, updated[k]);
+        }
+
+        data.view.$el.html("");
+        data.view.initialize();
+        data.view.$el.velocity("stop", true).css({
+          "background-color": "#ffff00"
+        }).velocity({
+          "backgroundColor": "#ffffff"
+        }, {duration: 3000 });
+
+      });
+      
+      // Hook up the validation indicator to update its 
+      // status whenever the editor changes
+      editor.on('change',function() {
+        // Get an array of errors from the validator
+        var errors = editor.validate();
+        
+        var indicator = document.getElementById('valid_indicator');
+        
+        // Not valid
+        if(errors.length) {
+          indicator.className = 'label alert';
+          indicator.textContent = 'not valid';
+        }
+        // Valid
+        else {
+          indicator.className = 'label success';
+          indicator.textContent = 'valid';
+        }
+      });
+});
